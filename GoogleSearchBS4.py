b4d887aa8660057fa966a51018040587b7d59b22
Mateus Rodrigues <68032415+mateus-rodriguess@users.noreply.github.com>
2021-04-29 12:34:13 -0300

    two commit

--- GoogleSearchBS4.py
+++ GoogleSearchBS4.py
@@ -4,6 +4,8 @@ import json
 from google_trans_new import google_translator 
 from time import sleep
 import json
+import pandas as pd
+
 
 # cabeçalho 
 headers = {
@@ -15,10 +17,10 @@ headers = {
 def translate_text(text):
     """
     function that receives a text is translated into 5 different languages. \n
-    portugues, ingles, chines tradiconal, frances, russo 
+    portugues, ingles, chines traditional, frances, russo 
     """
     translator = google_translator()
-    # portugues, ingles, chines tradiconal, frances, russo  
+    # portugues, ingles, chines tradicional, frances, russo  
     languages = ['pt', 'en', 'zh-CN', 'fr', 'ru']
     text_list = []
     
@@ -39,24 +41,38 @@ def beautiful_soup(search):
     function that searches for the translated phrase and returns a dict of results
     """
     summary = []
+    i = 0
+
     html = requests.get(f'https://www.google.com/search?q={search}',headers=headers).text
     soup = BeautifulSoup(html, 'lxml')
-
     for container in soup.findAll('div', class_='tF2Cxc'):
 
         heading = container.find('h3', class_='LC20lb DKV0Md').text
         article_summary = container.find('span', class_='aCOpRe').text
-        href =  container.find('div', class_='yuRUbf').text
+        
+        href =  container.find('div', class_='yuRUbf')
+        link = href.find('a')
 
         summary.append({
             'Heading': heading,
             'Article Summary': article_summary,
-            'href': href,
-        })
-
-    #print(json.dumps(summary, indent=4, ensure_ascii=False))
+            'href': link['href'],
+            })
     return summary
 
+
+def data_frame_search(search_list):
+    """
+    function that displays a dataframe of the search results in several languages
+    """
+    i =  0
+    languages = ['portugues', 'ingles', 'chines', 'frances', 'russo']
+    for lang in languages:
+        result = pd.DataFrame(search_list[i][lang], columns=["Heading","Article Summary", "href"])
+        print("-"*200)
+        print(result)
+        print("-" *200)
+        i += 1
 
 
 if __name__ == "__main__":
@@ -64,25 +80,23 @@ if __name__ == "__main__":
     main
     """
     i = 0
+    # pesquisa 
     search = "como criar uma função no python"
 
+    languages = ['portugues', 'ingles', 'chines', 'frances', 'russo']
     search_list = []
+
     text_list = translate_text(search)
     print(text_list)
-
-    languages = ['portugues', 'ingles', 'chines', 'frances', 'russo']
-   
     for text in text_list:
-        answer = beautiful_soup(text)
-        print(f">: {languages[i]}")
-        print(answer)
-        print("-" *200)
-        search_list.append({languages[i]: answer})
+        answer = beautiful_soup(text)   
+        search_list.append({languages[i]: answer})      
         i += 1
+    
+    data_frame_search(search_list)
 
-    print(json.dumps(search_list, indent=4, ensure_ascii=False))
+    # salvar em json
     with open("search.json", "w") as json_file:
-        # faz virar objeto do python
         json.dump(search_list, json_file, indent=4)
 
 
